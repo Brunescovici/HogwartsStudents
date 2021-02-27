19ed8ac7592fbf369c6e66589aedb14749cb0d2c
Brunescovici <andrei.pricope0@gmail.com>
2021-02-27 23:18:02 +0100

    finished student list

--- script.js
+++ script.js
@@ -7,29 +7,36 @@ fetch('https://petlatkea.dk/2021/hogwarts/students.json')
 function setStudents(data) {
     data.forEach(st => {
         let name = st.fullname.split(" "), firstnames = [], nick, firstname;
-        console.log(data.length);
         for(let i = 0; i<name.length; i++)
         {
-            console.log(name[i]);
-            name[i] = capFirstLetter(name[i]);
-            firstnames.push(name[i]);
+            if(name[i] != "") {
+                name[i] = capFirstLetter(name[i]);
+                firstnames.push(name[i]);
+            }
         }
-        if(firstnames[firstnames.length-1].search("\"")>=0)
+        if(firstnames.length > 2 && firstnames[firstnames.length-2].search("\"")>=0)
         {
-            nick = firstnames[firstnames.length-1].replace("\"", '');
-            firstnames.splice(firstnames.length-1, 1);
+            nick = firstnames[firstnames.length-2].replace("\"", '');
+            nick = nick.replace("\"",'');
+            nick = capFirstLetter(nick);
+            firstnames.splice(firstnames.length-2, 1);
         }
         firstname = firstnames[0];
-        for(let i = 1; i<firstnames.length; i++)
+        for(let i = 1; i<firstnames.length-1; i++)
             firstname = firstname + " " + firstnames[i];
-        if(nick>null)
-            student.push({firstname:firstname, nickname:nick});
+        let house = st.house.split(" ");
+        let i = 0;
+        while(house[i] == "")
+            i++;
+        house[i] = capFirstLetter(house[i]);
+        if(nick)
+            student.push({firstname:firstname, lastname:firstnames[firstnames.length-1], nickname:nick, gender:st.gender, house:house[i]});
         else
-            student.push({firstname:firstname});
+            student.push({firstname:firstname, lastname:firstnames[firstnames.length-1], gender:st.gender, house:house[i]});
     });
 }
 
 function capFirstLetter(s) {
-    s = s[0].toUpperCase() + s.slice(1);
+    s = s[0].toUpperCase() + s.slice(1).toLowerCase();
     return s;
 }
