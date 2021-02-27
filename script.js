4335fd2b7da8bef944c4fc9e469027018309af64
Brunescovici <andrei.pricope0@gmail.com>
2021-02-28 00:11:55 +0100

    added pictures, the blood type, and some other vars in the stud object

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
-let student = [];
+let student = [], halfBloods = [], pureBloods = [];
+
+fetch('https://petlatkea.dk/2021/hogwarts/families.json')
+    .then(response => response.json())
+    .then(data => setBloods(data));
+
+
 
 fetch('https://petlatkea.dk/2021/hogwarts/students.json')
     .then(response => response.json())
@@ -6,7 +12,7 @@ fetch('https://petlatkea.dk/2021/hogwarts/students.json')
 
 function setStudents(data) {
     data.forEach(st => {
-        let name = st.fullname.split(" "), firstnames = [], nick, firstname;
+        let name = st.fullname.split(/[ -]+/), firstnames = [], nick, firstname, lastname, bloodtype = 0, bt;
         for(let i = 0; i<name.length; i++)
         {
             if(name[i] != "") {
@@ -29,10 +35,36 @@ function setStudents(data) {
         while(house[i] == "")
             i++;
         house[i] = capFirstLetter(house[i]);
-        if(nick)
-            student.push({firstname:firstname, lastname:firstnames[firstnames.length-1], nickname:nick, gender:st.gender, house:house[i]});
+        lastname = firstnames[firstnames.length-1];
+        for(let j=0; j<halfBloods.length; j++)
+            if(halfBloods[j] == lastname)
+                bloodtype = 1; //it is half blood
+        for(let j=0; j<pureBloods.length; j++)
+            if(pureBloods[j] == lastname) {
+                if(bloodtype == 0)
+                    bloodtype = 2; //it is pure blood
+                else
+                    bloodtype = 3; //it is half-pure blood
+            }
+        if(bloodtype == 1)
+            bt = "Half Blood";
+        else if(bloodtype == 2)
+            bt = "Pure Blood";
         else
-            student.push({firstname:firstname, lastname:firstnames[firstnames.length-1], gender:st.gender, house:house[i]});
+            bt = "Half-pure Blood";
+        if(nick)
+            student.push({firstname:firstname, lastname:lastname, nickname:nick, gender:st.gender, house:house[i], bloodtype:bt, picture:lastname.toLowerCase() + "_" + firstname[0].toLowerCase(), prefect:0, expelled:0, inq:0});
+        else
+            student.push({firstname:firstname, lastname:lastname, gender:st.gender, house:house[i], bloodtype:bt, picture:lastname.toLowerCase() + "_" + firstname[0].toLowerCase(), prefect:0, expelled:0, inq:0});
+    });
+}
+
+function setBloods(data) {
+    data.half.forEach(blood => {
+        halfBloods.push(blood);
+    });
+    data.pure.forEach(blood => {
+        pureBloods.push(blood);
     });
 }
 
